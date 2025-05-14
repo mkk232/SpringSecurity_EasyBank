aa5a840c63c04201cf2aed5f02c7cc4d53352cc9
mkk232 <mkk232@lgupluspartners.co.kr>
2025-05-14 09:54:44 +0900

    initial commit

---
+++ src/test/java/com/mkk232/springsecurity_study/SpringSecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkk232.springsecurity_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
